eb4ba4698ad945b315238ede28f9c909e2cfb15f
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2023-09-13 17:39:27 +0900

    一からやる用のテンプレート

--- index.js
+++ index.js
@@ -13,38 +13,10 @@ let result = 0;
 //ボタンの数字
 let number = 0;
 
-cheat1();
+function numberButtonClick1() {}
 
-function numberButtonClick1() {
-  console.log(number);
-  displayNum = displayNum + number;
-  num1 = num1 + number;
-  resultInput.value = num1;
-}
+function operatorIfFunc(operator) {}
 
-function operatorIfFunc(operator) {
-  if (operator == "+") {
-    flag = true;
-    displayNum = displayNum + operator;
-    resultInput.value = displayNum;
-  }
-}
+function numberButtonClick2() {}
 
-function numberButtonClick2() {
-  displayNum = displayNum + number;
-  num2 = num2 + number;
-  resultInput.value = displayNum;
-}
-
-function calculate() {
-  if (operator == "+") {
-    result = Number(num1) + Number(num2);
-    resultInput.value = result;
-    flag = false;
-    num1 = result;
-    num2 = "";
-    displayNum = result;
-  }
-}
-operatorOtherFlag();
-calculateOtherFlag();
+function calculate() {}
