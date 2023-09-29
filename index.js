be869ab43c42b224943edd6e1af7a774550e4da1
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2023-09-30 08:23:19 +0900

    add "return"

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ async function operatorButton(operator) {
     const regex = /\+/;
     const ifValue = document.getElementById("result").value;
     if (regex.test(ifValue)) {
-      operatorIfFunc(operator);
+      return operatorIfFunc(operator);
     }
     if (operatorFlags) {
       operatorIfFuncOther(operator);
